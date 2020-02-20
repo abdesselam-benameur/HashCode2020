255d2505b8c6ba1e9f8e67e6c88f9389311b94fc
Nouredine <in_lamamra@esi.dz>
2020-02-20 19:24:31 +0100

    add library class

---
+++ src/Library.java
@@ -0,0 +1,19 @@
+
+public class Library {
+	private int nbBookPerDay;
+
+	public Library(int nbBookPerDay) {
+		super();
+		this.nbBookPerDay = nbBookPerDay;
+	}
+
+	public int getNbBookPerDay() {
+		return nbBookPerDay;
+	}
+
+	public void setNbBookPerDay(int nbBookPerDay) {
+		this.nbBookPerDay = nbBookPerDay;
+	}
+	
+	
+}
