24e9a75b8c5b3409162314008f8183ae2cb59ee9
Nouredine <in_lamamra@esi.dz>
2020-02-20 19:40:02 +0100

    library class all

--- src/Library.java
+++ src/Library.java
@@ -4,6 +4,22 @@ import java.util.HashSet;
 public class Library {
 	private int nbBookPerDay;
 	private HashSet<Book> setBooks ;
+	private int nbDays;
+	public HashSet<Book> getSetBooks() {
+		return setBooks;
+	}
+
+	public void setSetBooks(HashSet<Book> setBooks) {
+		this.setBooks = setBooks;
+	}
+
+	public int getNbDays() {
+		return nbDays;
+	}
+
+	public void setNbDays(int nbDays) {
+		this.nbDays = nbDays;
+	}
 
 	public Library(int nbBookPerDay) {
 		super();
